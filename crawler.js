c793534e061a4a637e74d7e6c18a913776c2d5ac
Starlight Exsper <exsper@163.com>
2020-07-19 12:32:34 +0800

    update

--- crawler.js
+++ crawler.js
@@ -16,7 +16,7 @@ class crawler {
             }
             let _data = '';
 
-            console.log("发送请求：" + requestOptions.host + requestOptions.path);
+            // console.log("发送请求：" + requestOptions.host + requestOptions.path);
 
             const req = https.request(requestOptions, function (res) {
                 res.setEncoding('utf8');
